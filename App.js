0c6c848d3f767455b562be093314829e05f868f1
marcosmcavalheiro <78524563+marcosmcavalheiro@users.noreply.github.com>
2022-03-28 01:12:42 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import React, {Component} from 'react';
 import './App.css';
 
-
 function App() {
 return(
 <div className="App">
       <header className="App-header">
-  
       </header>
     </div>
 );
 }export default App;
+
+
