dc7d2a70fd88da123cca741759e298464d662a44
marcosmcavalheiro <78524563+marcosmcavalheiro@users.noreply.github.com>
2022-03-28 01:06:00 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,13 +1,12 @@
 import React, {Component} from 'react';
 import './App.css';
-import Conversor from "./components/Conversor"
+
 
 function App() {
 return(
 <div className="App">
       <header className="App-header">
-    <Conversor moedaA="USD" moedaB="BRL"></Conversor>
-
+  
       </header>
     </div>
 );
