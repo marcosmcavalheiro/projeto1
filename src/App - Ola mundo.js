d71db43ee18bc88e0e7b5bbd6c113f373c8b2afb
marcosmcavalheiro <78524563+marcosmcavalheiro@users.noreply.github.com>
2022-03-29 00:37:45 -0300

    Add files via upload

---
+++ src/App - Ola mundo.js
@@ -0,0 +1,41 @@
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       Olá mundo !
+       
+      </header>
+    </div>
+  );
+}export default App;
+
+
+/* import logo from './logo.svg';
+ <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+<img src={logo} className="App-logo" alt="logo" />
+class HelloMessage extends React.Component {
+  render(){
+      return(
+          <div>
+              Olá, {this.props.name}!
+          </div>
+      )
+  }
+}
+
+ReactDOM.render(
+  <HelloMessage name="Mundo"/>,
+  document.getElementById('hello-example')
+);*/
+
